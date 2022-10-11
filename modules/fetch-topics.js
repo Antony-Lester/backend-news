25cfa10bb280a924ca79860d83c2c3943c4c9ca9
Antony Lester <Antony.lester.87@gmail.com>
2022-10-11 08:53:59 +0100

    Update fetch-topics.js

--- modules/fetch-topics.js
+++ modules/fetch-topics.js
@@ -1,6 +1,6 @@
 const db = require(`../db/connection`);
 
-module.exports = function fetchTopics () {
+module.exports = function fetchTopics() {
     return db.query(`SELECT * FROM topics;`)
         .then(({ rows: topics }) => {return topics})
 }

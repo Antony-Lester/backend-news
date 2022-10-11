03116d330f87e317874e98c31cf3c694864066d3
Antony Lester <Antony.lester.87@gmail.com>
2022-10-11 13:47:40 +0100

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const request = require('supertest');
 const db = require(`../db/connection`);
 const seed = require('../db/seeds/seed');
 const testData = require('../db/data/test-data');
-const { expect } = require('@jest/globals');
 
 beforeEach(() => seed(testData));
 afterAll(() => {
@@ -98,6 +97,7 @@ describe('get:', () => {
                 .get('/api/users')
                 .expect(200)
                 .then(res => {
+			expect(res._body.length).toBe(4)
                     res._body.forEach(news => {
                         expect(news).toEqual(
                             expect.objectContaining({

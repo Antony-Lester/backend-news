22fd49b2da90efd33ffd3e7bb71a7db157a67ce6
Antony Lester <Antony.lester.87@gmail.com>
2022-10-13 09:47:44 +0100

    Update fetch-articles.js

--- modules/fetch-articles.js
+++ modules/fetch-articles.js
@@ -1,16 +1,14 @@
 const db = require(`../db/connection`);
 
 module.exports = function fetchArticles(filter = null) {
-    const dbValues = [];
+    let dbValues = [];
     let dbRequest = `SELECT articles.*, comments.article_id, count(*)::int AS comment_count
     FROM comments 
     LEFT JOIN articles ON comments.article_id = articles.article_id
     JOIN users ON articles.author = users.username 
     GROUP BY comments.article_id, articles.article_id ` 
     if (filter) {
-        [filter] = Object.entries(filter)
-        dbValues.push(filter[0])
-        dbValues.push(filter[1])
+        [dbValues] = Object.entries(filter)
         dbRequest += `HAVING articles.$1 = '$2'`}
     dbRequest += `ORDER BY created_at DESC;`
     return db.query(dbRequest, dbValues)

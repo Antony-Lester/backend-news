8858a1387458c26a089b4faaeae2cc2da56dfda2
Antony Lester <Antony.lester.87@gmail.com>
2022-10-13 09:33:06 +0100

    Update fetch-articles.js

--- modules/fetch-articles.js
+++ modules/fetch-articles.js
@@ -1,22 +1,15 @@
 const db = require(`../db/connection`);
 
 module.exports = function fetchArticles(filter = null) {
-    if (filter) { [filter] = Object.entries(filter) }
-    const dbRequest = filter ? `SELECT articles.*, comments.article_id, count(*)::int AS comment_count
+    let dbRequest = `SELECT articles.*, comments.article_id, count(*)::int AS comment_count
     FROM comments 
     LEFT JOIN articles ON comments.article_id = articles.article_id
     JOIN users ON articles.author = users.username 
-    GROUP BY comments.article_id, articles.article_id
-    HAVING articles.${filter[0]} = '${filter[1]}'
-    ORDER BY created_at DESC
-    ;` :
-    `SELECT articles.*, comments.article_id, count(*)::int AS comment_count
-    FROM comments 
-    LEFT JOIN articles ON comments.article_id = articles.article_id
-    JOIN users ON articles.author = users.username 
-    GROUP BY comments.article_id, articles.article_id
-    ORDER BY created_at DESC
-    ;`
+    GROUP BY comments.article_id, articles.article_id ` 
+    if (filter) {
+        [filter] = Object.entries(filter)
+        dbRequest += `HAVING articles.${filter[0]} = '${filter[1]}'`}
+    dbRequest += `ORDER BY created_at DESC;`
     return db.query(dbRequest)
         .then(({ rows: articles }) => {
             if (articles.length === 0) {return Promise.reject({ code: 404 })}

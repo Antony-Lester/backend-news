6772a087146ff8222c5116132093530af20af84a
Antony-Lester <me@antonylester.com>
2022-11-02 20:58:32 +0000

    debugging

--- modules/set-comment.js
+++ modules/set-comment.js
@@ -1,9 +1,9 @@
 const db = require(`../db/connection`);
 
 module.exports = function setComment(article_id, data) {
-    //if (
-    //    typeof data.body !== "string") { return Promise.reject({ code: 400 }) }
-    //else {
+    if (
+        typeof data.body !== "string") { return Promise.reject({ code: 400 }) }
+    else {
         return db.query(
             `INSERT INTO comments (body, votes, author, article_id)
         VALUES ($1::TEXT, 0, $2, $3::INT)
@@ -12,5 +12,5 @@ module.exports = function setComment(article_id, data) {
             .then(({ rows: comment }) => {
                 return comment[0]
             })
-    //}
+    }
 };
